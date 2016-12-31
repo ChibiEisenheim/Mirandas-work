ed42f4947c8a03cf91288acb320ed7acf6d27372
Brian Friedhoff <brian.friedhoff@brianfriedhoff.customink.office>
2016-12-31 11:26:35 -0500

    updated...

--- wargameslol.rb
+++ wargameslol.rb
@@ -6,12 +6,13 @@ puts "And finally #{first_name}, your Last Name please..."
 last_name = gets.chomp
 
 playagain = true
-puts "Welcome #{first_name} #{middle_name} #{last_name}, Would you like to play a game?"
 while playagain
-  if gets.chomp.downcase == "yes"
+  puts "Welcome #{first_name} #{middle_name} #{last_name}, Would you like to play a game?"
+  response = gets.chomp.downcase
+  if response == "yes"
     puts "Please select a game from the list #{first_name}...\nSuper Kaizo World\nFriday the 14th\nPaper Jocky\nGlobal Thermonuclear War"
     playagain = false
-  elsif gets.chomp.downcase == "no"
+  elsif response == "no"
     puts "This machine will self destruct in 5 seconds..."
     puts "5..."
     sleep(1)
